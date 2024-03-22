38dcaa1bc9db990d31aa736622de0e2c6b0fe957
Mustafa Mokashi <neyaadeez@gmail.com>
2024-03-22 10:55:01 -0700

    test

--- malloc.c
+++ malloc.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+void func(){
+    char var[] = "this value is hidden";
+    char ch[] = "hello world";
+    printf("%s", ch);
+}
 int main(){
     // int *pointer;
     //int *pointer2;
 
 
-    int *pointer =(int *) malloc(5* sizeof(int));
+    char *pointer =(char *) malloc(5* sizeof(char));
     // pointer2 = malloc(100* sizeof(int));
     // pointer = calloc(5, sizeof(int));
     if(pointer == NULL){
@@ -24,7 +29,8 @@ int main(){
         // printf("pointer2 inc value: %p\n", ++pointer2);
         // printf("pointer value: %p\n", pointer);
         // printf("pointer inc value: %p\n", ++pointer);
-
+        func();
+        scanf("%s", pointer);
         printf(pointer);
     }
     return 0;
